6ce8456cb27c68c270cf1edc9e25c28b71441115
abdul1ah <almamun01817@gmail.com>
2023-06-10 14:33:03 +0600

    patch

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const verifyJWT = (req,res,next) => {
+const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
   if (!authorization) {
     res.status(401).send({ error: true, message: 'unauthorized function' })
@@ -103,11 +103,11 @@ async function run() {
       const query = { _id: new ObjectId(id) }
       const doc = {
         $set: {
-         status: 'approve'
+          status: 'approve'
         }
       }
       const option = { upsert: true }
-      const result = await classesCollection.updateOne(query, doc,option)
+      const result = await classesCollection.updateOne(query, doc, option)
       res.send(result)
     })
 
@@ -118,11 +118,11 @@ async function run() {
       const query = { _id: new ObjectId(id) }
       const doc = {
         $set: {
-         status: 'denied'
+          status: 'denied'
         }
       }
       const option = { upsert: true }
-      const result = await classesCollection.updateOne(query, doc,option)
+      const result = await classesCollection.updateOne(query, doc, option)
       res.send(result)
     })
 
@@ -132,11 +132,11 @@ async function run() {
       const query = { _id: new ObjectId(id) }
       const doc = {
         $set: {
-         status: 'denied'
+          status: 'denied'
         }
       }
       const option = { upsert: true }
-      const result = await classesCollection.updateOne(query, doc,option)
+      const result = await classesCollection.updateOne(query, doc, option)
       res.send(result)
     })
 
@@ -163,13 +163,13 @@ async function run() {
     })
 
 
-    app.get('/classes',async(req,res)=>{
-      const status=req.query.status;
-      if(!status){
+    app.get('/classes', async (req, res) => {
+      const status = req.query.status;
+      if (!status) {
         res.send([])
       }
-      const query={status:status}
-      const result=await classesCollection.find(query).toArray()
+      const query = { status: status }
+      const result = await classesCollection.find(query).toArray()
       res.send(result)
     })
 
@@ -197,8 +197,8 @@ async function run() {
       console.log(result);
     })
 
-    app.post('/purchase',async(req,res)=>{
-      const body=req.body;
+    app.post('/purchase', async (req, res) => {
+      const body = req.body;
       res.send(await purchaseCollection.insertOne(body))
     })
 
@@ -206,27 +206,18 @@ async function run() {
       const result = await purchaseCollection.find().toArray()
       res.send(result)
     })
-    // app.post('/purchase', async (req, res) => {
-    //   const user = req.body;
-    //   const result = await purchaseCollection.insertOne(user)
-    //   res.send(result)
-    // })
-    // booking collection api 
 
-    // app.put('/class/:id', async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) }
-    //   const doc = {
-    //     $set: {
-    //       status: 'approve'
-    //     }
-    //   }
+    // purchase deleted api 
 
-    //   const result = await classesCollection.updateOne(query, doc)
-    //   res.send(result)
+    app.delete('/purchase/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      res.send(await purchaseCollection.deleteOne(query))
+    })
 
-    // })
-    // Connect the client to the server	(optional starting in v4.7)
+
+
+
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
